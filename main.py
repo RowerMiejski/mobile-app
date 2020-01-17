534fbc21637cfbe0d9286b266d4a987a45f22ee0
RowerMiejski <Rowerkuby123@gmail.com>
2020-01-17 01:53:48 +0100

    working app, sending commands

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 import RabbitMQ as rb
-/import threading
-
+import threading
 
 message = "jebanko"
 server = rb.Server()
@@ -14,13 +13,15 @@ server.ReadConfig("serwer", True)
 
 class MyGui(FloatLayout):
     console = ObjectProperty(None)
-    id = ObjectProperty(None)
-    debug = ObjectProperty
+    idBox = ObjectProperty()
+    debug = ObjectProperty()
     id1 = "ID :"
-    debug1 = "Podaj ID maszyny"
+    debug1 = "Podaj swoje imie"
     command = ""
     queue = ""
+    isInQueue = False
     whileCommand = False
+    isNeeded = False
 
     def __init__(self, **kwargs):
         super(MyGui,self).__init__(**kwargs)
@@ -34,44 +35,64 @@ class MyGui(FloatLayout):
         self.console.text = ""
 
     def interpretCommand(self):
-        words = self.command.split()
+        self.words = self.command.split()
         self.whileCommand = True
-        if len(words)>1 and str(words[0].lower()) == "connect":
-            self.changeQueue(words[1])
-        elif len(words)>1 and str(words[0].lower()) == "giveid":
-            self.giveID(words[1])
-        elif str(words[0].lower()) == "!reset":
-            self.giveID(words[1])
+        if len(self.words) > 1 and str(self.words[0].lower()) == "connect":
+            self.changeQueue(self.words[1])
+        elif len(self.words) > 1 and str(self.words[0].lower()) == "giveid":
+            self.giveID(self.words[1])
+        elif str(self.words[0].lower()) == "cls":
+            self.debug.text = ""
+        elif str(self.words[0].lower()) == "send" and len(self.words) > 1:
+            if self.isInQueue:
+                self.sendCommand(self.words[1].lower(), self.words[2])
+            else:
+                self.debug.text += " - BLAD, nie wybrales kolejki.\n"
         else:
             self.debugUpdate(True)
 
 
 
     def changeQueue(self, args):
-        queue = str(args)
-        print(queue)
-        server.Write("kacper knuciarz jebany", queue)
+        self.queue = str(args)
+        self.idBox.text = ""
+        self.idBox.text = "ID: " + self.queue
         self.debugUpdate(False)
-
+        self.isInQueue = True
 
     def debugUpdate(self, didFail):
         if didFail:
-            self.debug.text += ", command failed."
+            self.debug.text += "- komenda zakonczona niepowodzeniem."
         else:
-            self.debug.text += ", command done succesfully."
-
+            self.debug.text += "- komenda zakonczona powodzeniem."
 
     def idUpdate(self):
         serverID.ReadConfig("serwer", True)
+        server.ReadConfig("kuba", True)
+
         if str(serverID.Read()) == "needID":
-            print("jebanko disa powieksa penisa")
-            if not self.whileCommand:
-                self.debug.text += "\n\n URZADZENIE PROSI O ID, WPISZ KOMENDE giveid + ID"
-            else:
-                self.debug.text += "\n\n URZADZENIE PROSI O ID, WPISZ KOMENDE !reset, aby zresetowac polaczenie a nastepnie giveid + ID"
+            self.isNeeded = True
+            self.debug.text += " \nURZADZENIE PROSI O ID, WPISZ KOMENDE giveid + ID"
+            self.idUpdate()
+        elif str(server.Read()) != "":
+            formattedOutput = []
+            formattedOutput = str(server.Read()).split()
+            print(formattedOutput)
+            self.debug.text += str(formattedOutput)
+            self.idUpdate()
 
     def giveID(self, ID):
-        serverID.Write(ID, "serwer")
+        if self.isNeeded:
+            serverID.Write(ID, "serwer_response")
+            self.debugUpdate(False)
+            self.isNeeded = False
+        else:
+            self.debug.text += " - Nie otrzymano komendy needID"
+            self.isNeeded = False
+
+    def sendCommand(self, command, args):
+        server.Write("kuba" + " " + command + " " + args, self.queue)
+        self.debugUpdate(False)
 
 
 class MyApp(App):
@@ -82,6 +103,7 @@ class MyApp(App):
 def start():
     if __name__ == "__main__":
         MyApp().run()
+
 
 t2 = threading.Thread(target=start)
 t2.start()
